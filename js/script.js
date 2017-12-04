4745a59c3694affcfc81e3626ab35361aff1fc9f
saurav <sauravbhagat6969@gmail.com>
2017-12-04 11:58:26 +0530

    Form layout done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-(function(){
-	console.log("saurav");
-}())
+$(document).ready(function(){
+
+	$('#sub_btn').on('click', function(e) {
+		e.preventDefault();
+		// $('.first-section').addClass('hidee');
+		// $('.second-section').removeClass('hidee');
+		$('.first-section').fadeOut("fast",function(){
+			$('.second-section').fadeIn("fast");
+		});
+		
+		console.log("kjbsdvkjsd");
+	});
+	$('#change_category').on('click', function(e){
+		$('.second-section').fadeOut("fast",function(){
+			$('.first-section').fadeIn("fast");
+		});
+	});
+	$('#cross_icon').on('click', function(){
+		$('.second-section').fadeOut("fast",function(){
+			$('.first-section').fadeIn("fast");
+		});
+	});
+
+});
+
+
+// second-section =664px
+//form-body = 660px
+//tech-form = 502px
